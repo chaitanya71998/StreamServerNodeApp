42bf13182c920f705af2807c8781e97a05bafc30
chaitanya71998 <chaitanya.terli@nxtwave.co.in>
2022-09-14 16:17:00 +0530

    add version one

--- index.js
+++ index.js
@@ -1,21 +1,17 @@
-// const express = require("express")
-// const app = express()
-
-// app.listen(3001)
-
-const express = require("express") //Import the express dependency
-const app = express() //Instantiate an express app, the main work horse of this server
-const port = 5000 //Save the port number where your server will be listening
+const express = require("express")
+const app = express()
+const port = 5000
+var cors = require("cors")
 let stream = require("getstream")
+
+app.use(cors())
 app.use(express.json())
-//Idiomatic expression in express to route and respond to a client request
-app.get("/", (req, res) => {
-  //get requests to the root ("/") will route here
-  res.sendFile("index.html", { root: __dirname }) //server responds by sending the index.html file to the client's browser
-  //the .sendFile method needs the absolute path to the file, see: https://expressjs.com/en/4x/api.html#res.sendFile
+
+app.get("/", (request, response) => {
+  res.sendFile("index.html", { root: __dirname })
 })
 
-app.get("/getTokenFromUser", async (request, response) => {
+app.post("/getTokenFromUser/", async (request, response) => {
   // Instantiate a new client (client side)
   try {
     let client = stream.connect(
@@ -23,15 +19,23 @@ app.get("/getTokenFromUser", async (request, response) => {
       "ts9ke7dw3kqzzfq3z6zzwv8pedhhfd68m7vj6xkxywb3mbs4myhwwk76qrpjudau",
       "1209187"
     )
-    const { userName } = request
-    // Find your API keys here https://getstream.io/dashboard/
-    let userToken = client.createUserToken(userName)
+    const { data } = request.body
+    const { user_name } = JSON.parse(JSON.parse(data))
+    console.log(user_name, "username")
+
+    let userToken = client.createUserToken(`${user_name}`)
+
+    // to make user follow the coding practice
+    userFeed = client.feed("timeline", user_name, userToken)
+    await userFeed.follow("codingPractice", "cp_1")
 
     response.send({ user_token: userToken })
-  } catch (e) {}
+  } catch (e) {
+    console.log(e)
+  }
 })
 
-app.post("/addBatchFeed", async (request, response) => {
+app.post("/addBatchFeed/", async (request, response) => {
   try {
     let client = stream.connect(
       "wwfzfuma9356",
@@ -39,11 +43,18 @@ app.post("/addBatchFeed", async (request, response) => {
       "1209187"
     )
     const feeds = ["codingPractice:cp_1"]
-    const { activity } = request.body
+    const { data } = request.body
+
+    const { activity } = JSON.parse(JSON.parse(data))
+    console.log(activity, "activity")
+
     const success = await client.addToMany(activity, feeds)
     response.send({})
-  } catch (e) {}
+  } catch (e) {
+    response.send(e)
+  }
 })
+
 app.listen(port, () => {
   //server starts listening for any attempts from a client to connect at port: {port}
   console.log(`Now listening on port ${port}`)
